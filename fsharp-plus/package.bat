54fae9deda2001e861316faca870fb7c330fb982
Edward Yang <qwbarch@gmail.com>
2024-01-29 05:08:40 -0500

    Add FSharpPlus

---
+++ fsharp-plus/package.bat
@@ -0,0 +1,10 @@
+@echo off
+
+powershell Compress-Archive^
+    -Force^
+    -Path "BepInEx/core/",^
+          "LICENSE",^
+          "manifest.json",^
+          "README.md",^
+          "icon.png"^
+    -DestinationPath "FSharp.Plus.zip"
